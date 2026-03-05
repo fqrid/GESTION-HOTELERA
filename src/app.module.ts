767ce5acbecb91d84051712e72f0fd6c5c8de2f9
Farid Zemanate <faridzemanate@gmail.com>
2026-03-04 21:34:25 -0500

    Ajustes finales en Guest y AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,7 +6,7 @@ import { GuestModule } from './guest/guest.module';
   imports: [
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: 'localhost',
+      host: '127.0.0.1', 
       port: 3306,
       username: 'root',
       password: '',
--- src/guest/dto/create-guest.dto.ts
+++ src/guest/dto/create-guest.dto.ts
@@ -1,6 +1,16 @@
+import { IsString, IsEmail } from 'class-validator';
+
 export class CreateGuestDto {
+
+  @IsString()
   name: string;
+
+  @IsString()
   document: string;
+
+  @IsString()
   phone: string;
+
+  @IsEmail()
   email: string;
 }
